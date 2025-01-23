85a67ecd7bd675f24163f43a9c16648f8f91bb9b
Tomk99 <tcsavas99@gmail.com>
2025-01-23 14:15:56 +0100

    -

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,13 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://jw-kisokos-aa53f6432494.herokuapp.com',
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-export const getAllCards = () => api.get('/api/cards', {headers: { 'Content-Type': 'application/json'}});
+export const getAllCards = () => api.get('/api/cards');
 export const createCard = (data) =>
   api.post('/api/cards', data, { headers: { 'Content-Type': 'multipart/form-data' } });
 export const deleteCard = (id) => api.delete(`/api/cards/${id}`);
