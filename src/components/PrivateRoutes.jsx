b3f60030f8f5ddd5a9b3c5d57b6daa6a0c12b022
Tomk99 <tcsavas99@gmail.com>
2025-01-23 10:32:58 +0100

    fix api path

--- src/components/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoutes = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoutes;
