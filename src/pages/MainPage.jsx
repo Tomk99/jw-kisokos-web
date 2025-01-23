d4b61f23247988d19def94220e282b91275678c5
Tomk99 <tcsavas99@gmail.com>
2025-01-23 10:49:50 +0100

    Google Auth

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,7 +8,6 @@ const MainPage = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // Felhasználói adatok lekérése a backendről
     api.get('/api/user')
       .then(response => setUser(response.data))
       .catch(error => console.error('Hiba a felhasználói adatok lekérésekor:', error));
@@ -20,7 +19,6 @@ const MainPage = () => {
         <h1>JW Kisokos</h1>
         {user && (
           <div className="user-info">
-            <img src={user.picture} alt="Profilkép" className="profile-image" />
             <div>
               <p className="user-name">{user.name}</p>
               <p className="user-email">{user.email}</p>
