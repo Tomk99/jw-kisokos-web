1f8bc74eda9ade1dd4313d20f78057a163b195f3
Tomk99 <tcsavas99@gmail.com>
2025-01-23 14:13:20 +0100

    -

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   withCredentials: true
 });
 
-export const getAllCards = () => api.get('/api/cards');
+export const getAllCards = () => api.get('/api/cards', {headers: { 'Content-Type': 'application/json'}});
 export const createCard = (data) =>
   api.post('/api/cards', data, { headers: { 'Content-Type': 'multipart/form-data' } });
 export const deleteCard = (id) => api.delete(`/api/cards/${id}`);
